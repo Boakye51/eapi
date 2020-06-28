8f8b131781d2a527bb2f431f4db6522f34244571
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-06-28 14:37:37 +0000

    Created Mode -a

---
+++ database/factories/ReviewsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Reviews;
+use Faker\Generator as Faker;
+
+$factory->define(Reviews::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
