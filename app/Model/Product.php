e03ca46a3471261584e00e5d89943468820acbd6
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-06-28 16:05:12 +0000

    Product and Reviews Relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Reviews;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Reviews::class);
+    }
 }
